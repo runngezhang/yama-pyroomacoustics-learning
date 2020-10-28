48cb495eec0cb4bf56857e073e60fbb262bc5495
kurikinton105 <yamadakenta0101@gmail.com>
2020-10-28 23:52:33 +0900

    ドキュメントの作成

--- python_source_separation-master/section2/reverse.py
+++ python_source_separation-master/section2/reverse.py
@@ -9,6 +9,25 @@ import sounddevice as sd
 #matplotlib
 import matplotlib.pyplot as plt
 
+# STFTを自前で実装してみる
+# [INPUT]
+#     sig: input signal (len x 1) 入力信号
+#      fs: サンプリング周波数(wav.getframerate()で取得可能)
+#   wndow: analysis window function (frlen x 1, default: hamming window) 窓関数(wid)
+#  nperseg: frame length (even number, must be dividable by frsft) フレーム長(frlen)
+# noverlap: frame shift  (default: frlen/2) フレームシフト(frsft)
+# ex) STFT(data,fs=wav.getframerate(),window="hann",nperseg=512,noverlap=256)
+# [OUTPUT]
+# f : 出力されるデータの周波数軸の各インデックスの具体的な周波数〔Hz〕
+# t : フレーム軸の各インデックスの具体的な時間〔sec〕
+#  stft_data: STFT of input signal (frlen/2+1 x nf) STFTの信号 短時間 フーリエ変換後の複素数の信号 y(l, k)
+
+def STFT(sig,frlen,frsft,wnd):
+
+    return sig
+
+
+
 #読み込むサンプルファイル
 sample_wave_file="/Users/kenta/Programing/github/yama-pyroomacoustics-learning/python_source_separation-master/section2/CMU_ARCTIC/cmu_us_axb_arctic/wav/arctic_a0001.wav"
 #ファイルを読み込む
